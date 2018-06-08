574eaed813caf822d42d29bc3a80be93073b38c7
zlindaz2411 <zlindaz2411@gmail.com>
2018-06-08 18:45:56 +0200

    Refactor

--- guess word.py
+++ guess word.py
@@ -3,275 +3,99 @@ from tkinter import messagebox
 import random
 import sys
 
-count =0
+#2 things left: countNot Guessed and countGuessed
+
+class Counter(object):
+        countNotGuessed = 0
+        countGuessed = 0
+
 #EVENTS:
 def make_button():
-    def set_textA(event: NONE):
-            for b in buttons:
-                if b.get_content() == "A":
-                    b['text'] = "A"
-                if b.get_content()!="A":
-                    global count
-                    count +=1
-            print(count)
-    def set_textB(event: NONE):
-            for b in buttons:
-                if b.get_content() == "B":
-                    b['text'] = "B"
-                else:
-                    global count
-                    count+=1
-    def set_textC(event: NONE):
-            for b in buttons:
-                if b.get_content() == "C":
-                    b['text'] = "C"
-                else:
-                    global count
-                    count+=1
-    def set_textD(event: NONE):
-            for b in buttons:
-                if b.get_content() == "D":
-                    b['text'] = "D"
-                else:
-                    global count
-                    count+=1
-    def set_textE(event: NONE):
-            for b in buttons:
-                if b.get_content() == "E":
-                    b['text'] = "E"
-                else:
-                    global count
-                    count+=1
-    def set_textF(event: NONE):
-            for b in buttons:
-                if b.get_content() == "F":
-                    b['text'] = "F"
-                else:
-                    global count
-                    count+=1
-    def set_textG(event: NONE):
-            for b in buttons:
-                if b.get_content() == "G":
-                    b['text'] = "G"
-                else:
-                    global count
-                    count+=1
-    def set_textH(event: NONE):
-            for b in buttons:
-                if b.get_content() == "H":
-                    b['text'] = "H"
-                else:
-                    global count
-                    count+=1
-    def set_textI(event: NONE):
-            for b in buttons:
-                if b.get_content() == "I":
-                    b['text'] = "I"
-                else:
-                    global count
-                    count+=1
-    def set_textJ(event: NONE):
-            for b in buttons:
-                if b.get_content() == "J":
-                    b['text'] = "J"
-                else:
-                    global count
-                    count+=1
-    def set_textK(event: NONE):
-            for b in buttons:
-                if b.get_content() == "K":
-                    b['text'] = "K"
-                else:
-                    global count
-                    count+=1
-    def set_textL(event: NONE):
-            for b in buttons:
-                if b.get_content() == "L":
-                    b['text'] = "L"
-                else:
-                    global count
-                    count+=1
-    def set_textM(event: NONE):
-            for b in buttons:
-                if b.get_content() == "M":
-                    b['text'] = "M"
-                else:
-                    global count
-                    count+=1
-    def set_textN(event: NONE):
-            for b in buttons:
-                if b.get_content() == "N":
-                    b['text'] = "N"
-                else:
-                    global count
-                    count+=1
-    def set_textO(event: NONE):
-            for b in buttons:
-                if b.get_content() == "O":
-                    b['text'] = "O"
-                else:
-                    global count
-                    count+=1
-    def set_textP(event: NONE):
-            for b in buttons:
-                if b.get_content() == "P":
-                    b['text'] = "P"
-                else:
-                    global count
-                    count+=1
-    def set_textQ(event: NONE):
-            for b in buttons:
-                if b.get_content() == "Q":
-                    b['text'] = "Q"
-                else:
-                    global count
-                    count+=1
-    def set_textR(event: NONE):
-            for b in buttons:
-                if b.get_content() == "R":
-                    b['text'] = "R"
-                else:
-                    global count
-                    count+=1
-    def set_textS(event: NONE):
-            for b in buttons:
-                if b.get_content() == "S":
-                    b['text'] = "S"
-                else:
-                    global count
-                    count+=1
-    def set_textT(event: NONE):
-            for b in buttons:
-                if b.get_content() == "T":
-                    b['text'] = "T"
-                else:
-                    global count
-                    count+=1
-    def set_textU(event: NONE):
-            for b in buttons:
-                if b.get_content() == "U":
-                    b['text'] = "U"
-                else:
-                    global count
-                    count+=1
-    def set_textV(event: NONE):
-            for b in buttons:
-                if b.get_content() == "V":
-                    b['text'] = "V"
-                else:
-                    global count
-                    count+=1
-    def set_textW(event: NONE):
-            for b in buttons:
-                if b.get_content() == "W":
-                    b['text'] = "W"
-                else:
-                    global count
-                    count+=1
-    def set_textX(event: NONE):
-            for b in buttons:
-                if b.get_content() == "X":
-                    b['text'] = "X"
-                else:
-                    global count
-                    count+=1
-    def set_textY(event: NONE):
-            for b in buttons:
-                if b.get_content() == "Y":
-                    b['text'] = "Y"
-                else:
-                    global count
-                    count+=1
-    def set_textZ(event: NONE):
-            for b in buttons:
-                if b.get_content() == "Z":
-                    b['text'] = "Z"
-                else:
-                    global count
-                    count+=1
-    buttonA = Button(frame2, text="A", width=10)
-    buttonA.bind("<Button-1>", set_textA)
+
+    def check_lose_win():
+        if (Counter.countNotGuessed > 5):
+            messagebox.showwarning("You lose", "You guessed wrong more than 5 times, you LOSE, haha, dumbass")
+            sys.exit()
+        if(Counter.countGuessed > len(random_word)-1):
+            messagebox.showwarning("Congratulations", "You guessed the word, you WIN!")
+            sys.exit()
+    def increase_count():
+        Counter.countNotGuessed += 1
+        count_string.set(str(Counter.countNotGuessed))
+
+    def set_text(button):
+        for b in buttons:
+            if b.get_content() == button['text']:
+                b['text'] = b.get_content()
+                Counter.countGuessed += random_word.count(button["text"])
+            else:
+                increase_count()
+        #check_lose_win
+
+    buttonA = Button(frame2, text="A", width=10, command = lambda: set_text(buttonA))
     buttonA.grid(row=3, column=0, sticky=W)
-    buttonB = Button(frame2, text="B", width=10)
-    buttonB.bind("<Button-1>", set_textB)
+    buttonB = Button(frame2, text="B", width=10, command = lambda: set_text(buttonB))
     buttonB.grid(row=3, column=1, sticky=W)
-    buttonC = Button(frame2, text="C", width=10)
-    buttonC.bind("<Button-1>", set_textC)
+    buttonC = Button(frame2, text="C", width=10, command = lambda: set_text(buttonC))
     buttonC.grid(row=3, column=2, sticky=W)
-    buttonD = Button(frame2, text="D", width=10)
-    buttonD.bind("<Button-1>", set_textD)
+    buttonD = Button(frame2, text="D", width=10, command = lambda: set_text(buttonD))
     buttonD.grid(row=3, column=3, sticky=W)
-    buttonE = Button(frame2, text="E", width=10)
-    buttonE.bind("<Button-1>", set_textE)
+    buttonE = Button(frame2, text="E", width=10, command = lambda: set_text(buttonE))
     buttonE.grid(row=3, column=4, sticky=W)
-    buttonF = Button(frame2, text="F", width=10)
-    buttonF.bind("<Button-1>", set_textF)
+    buttonF = Button(frame2, text="F", width=10, command = lambda: set_text(buttonF))
     buttonF.grid(row=3, column=5, sticky=W)
-    buttonG = Button(frame2, text="G", width=10)
-    buttonG.bind("<Button-1>", set_textG)
+    buttonG = Button(frame2, text="G", width=10, command = lambda: set_text(buttonG))
     buttonG.grid(row=3, column=6, sticky=W)
-    buttonH = Button(frame2, text="H", width=10)
-    buttonH.bind("<Button-1>", set_textH)
+    buttonH = Button(frame2, text="H", width=10, command = lambda: set_text(buttonH))
     buttonH.grid(row=4, column=0, sticky=W)
-    buttonI = Button(frame2, text="I", width=10)
-    buttonI.bind("<Button-1>", set_textI)
+    buttonI = Button(frame2, text="I", width=10, command = lambda: set_text(buttonI))
     buttonI.grid(row=4, column=1, sticky=W)
-    buttonJ = Button(frame2, text="J", width=10)
-    buttonJ.bind("<Button-1>", set_textJ)
+    buttonJ = Button(frame2, text="J", width=10, command = lambda: set_text(buttonJ))
     buttonJ.grid(row=4, column=2, sticky=W)
-    buttonK = Button(frame2, text="K", width=10)
-    buttonK.bind("<Button-1>", set_textK)
+    buttonK = Button(frame2, text="K", width=10, command = lambda: set_text(buttonK))
     buttonK.grid(row=4, column=3, sticky=W)
-    buttonL = Button(frame2, text="L", width=10)
-    buttonL.bind("<Button-1>", set_textL)
+    buttonL = Button(frame2, text="L", width=10, command = lambda: set_text(buttonL))
     buttonL.grid(row=4, column=4, sticky=W)
-    buttonM = Button(frame2, text="M", width=10)
-    buttonM.bind("<Button-1>", set_textM)
+    buttonM = Button(frame2, text="M", width=10, command = lambda: set_text(buttonM))
     buttonM.grid(row=4, column=5, sticky=W)
-    buttonN = Button(frame2, text="N", width=10)
-    buttonN.bind("<Button-1>", set_textN)
+    buttonN = Button(frame2, text="N", width=10, command = lambda: set_text(buttonN))
     buttonN.grid(row=4, column=6, sticky=W)
-    buttonO = Button(frame2, text="O", width=10)
-    buttonO.bind("<Button-1>", set_textO)
+    buttonO = Button(frame2, text="O", width=10, command = lambda: set_text(buttonO))
     buttonO.grid(row=5, column=0, sticky=W)
-    buttonP = Button(frame2, text="P", width=10)
-    buttonP.bind("<Button-1>", set_textP)
+    buttonP = Button(frame2, text="P", width=10, command = lambda: set_text(buttonP))
     buttonP.grid(row=5, column=1, sticky=W)
-    buttonQ = Button(frame2, text="Q", width=10)
-    buttonQ.bind("<Button-1>", set_textQ)
+    buttonQ = Button(frame2, text="Q", width=10, command = lambda: set_text(buttonQ))
     buttonQ.grid(row=5, column=2, sticky=W)
-    buttonR = Button(frame2, text="R", width=10)
-    buttonR.bind("<Button-1>", set_textR)
+    buttonR = Button(frame2, text="R", width=10, command = lambda: set_text(buttonR))
     buttonR.grid(row=5, column=3, sticky=W)
-    buttonS = Button(frame2, text="S", width=10)
-    buttonS.bind("<Button-1>", set_textS)
+    buttonS = Button(frame2, text="S", width=10, command = lambda: set_text(buttonS))
     buttonS.grid(row=5, column=4, sticky=W)
-    buttonT = Button(frame2, text="T", width=10)
-    buttonT.bind("<Button-1>", set_textT)
+    buttonT = Button(frame2, text="T", width=10, command = lambda: set_text(buttonT))
     buttonT.grid(row=5, column=5, sticky=W)
-    buttonU = Button(frame2, text="U", width=10)
-    buttonU.bind("<Button-1>", set_textU)
+    buttonU = Button(frame2, text="U", width=10, command = lambda: set_text(buttonU))
     buttonU.grid(row=5, column=6, sticky=W)
-    buttonV = Button(frame2, text="V", width=10)
-    buttonV.bind("<Button-1>", set_textV)
+    buttonV = Button(frame2, text="V", width=10, command = lambda: set_text(buttonV))
     buttonV.grid(row=6, column=0, sticky=W)
-    buttonW = Button(frame2, text="W", width=10)
-    buttonW.bind("<Button-1>", set_textW)
+    buttonW = Button(frame2, text="W", width=10, command = lambda: set_text(buttonW))
     buttonW.grid(row=6, column=1, sticky=W)
-    buttonX = Button(frame2, text="X", width=10)
-    buttonX.bind("<Button-1>", set_textX)
+    buttonX = Button(frame2, text="X", width=10, command = lambda: set_text(buttonX))
     buttonX.grid(row=6, column=2, sticky=W)
-    buttonY = Button(frame2, text="Y", width=10)
-    buttonY.bind("<Button-1>", set_textY)
+    buttonY = Button(frame2, text="Y", width=10, command = lambda: set_text(buttonY))
     buttonY.grid(row=6, column=3, sticky=W)
-    buttonZ = Button(frame2, text="Z", width=10)
-    buttonZ.bind("<Button-1>", set_textZ)
+    buttonZ = Button(frame2, text="Z", width=10, command = lambda: set_text(buttonZ))
     buttonZ.grid(row=6, column=4, sticky=W)
+    label_wrong= Label(frame2, text= "Wrong: ").grid(row=6,column =5, sticky =W)
+    count_string = StringVar()
+    count_string.set(str(Counter.countNotGuessed))
+    label_count= Label(frame2, textvariable =count_string).grid(row =6, column = 6, sticky=W)
 
 #POPULATE:
 list = ["ELEPHANT", "MATHEMATICS", "COMPUTER", "BAZINGA", "ZIGZAG", "YOLO", "SNAKE",
         "CAT", "MEOW", "WORLD", "JOHN", "SNOW", "THRONE", "DRAGON", "FRIENDS",
-        "HOUSE", "LONDON", "THEORY", "WEIGHT", "SCIENTIST", "WINTER", "AUTUMN"]
+        "HOUSE", "LONDON", "THEORY", "WEIGHT", "SCIENTIST", "WINTER", "AUTUMN", "PIANO", "PALACE",
+        "INCOMPREHENSIBILITY", "INTELLIGENCE", "WOLVES", "MOTHER", "FATHER", "PSYCHOLOGY",
+        "PHILOSOPHY", "UNCOPYRIGHTABLE", "SPONGEBOB", "ANOMALISTIC", "ACCOUNTING", "MAGNANIMOUS",
+        "PENULTIMATE", "PERFIDIOUSNESS", "DUMBASS", "WINEBIBBER", "DRUG", "PAPRIKA"]
 random_word = random.choice(list)
 
 
@@ -280,7 +104,7 @@ root = Tk()
 
 frame = Frame(root)
 frame2 = Frame(root)
-title_label = Label(root, text="Guess the word. Press letter or word to select the mode", width=70).pack()
+title_label = Label(root, text="Guess the word").pack()
 
 class ButtonExtended(Button):
     def __init__(self, content = "", *args, **kwargs):
@@ -293,30 +117,16 @@ class ButtonExtended(Button):
 
 buttons = []
 for i in range(len(random_word)):
-    button = ButtonExtended(random_word[i],frame, text ="", width = 5)
+    button = ButtonExtended(random_word[i],frame, text = "", width = 5)
     button.grid(row =0, column = i)
     buttons.append(button)
 
 make_button()
 
-buttonLetter = Button(frame2, text="Letter", width=10)\
-    .grid(row=6, column=5, sticky=W)
-buttonWord = Button(frame2, text="Word", width=10)\
-    .grid(row=6, column=6, sticky=W)
-
-
 
 frame.pack(side = TOP)
 frame2.pack()
 
+
 root.mainloop()
 
-#GAME:
-word_guessed = False
-while(not word_guessed and count <=5):
-    if(count >5):
-     messagebox.showwarning("You lose", "You guessed wrong more than 5 times, you LOSE")
-     sys.exit()
-    if(word_guessed):
-     messagebox.showwarning("Congratulations", "You guessed the word, you WIN!")
-     sys.exit()
